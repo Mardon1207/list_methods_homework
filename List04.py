d5854c37c483eab80f0fd3c6f481819240901dda
Mardon1207 <mardonsultonov8@gmail.com>
2023-04-27 23:36:39 -0700

    4

--- List04.py
+++ List04.py
@@ -7,4 +7,8 @@ def main(numbers,i):
     Returns:
         list: return answer
     """
-    return
+    fruits.pop(i)
+    return fruits
+fruits=[1,2,3,4,5,6,7,8]
+i=int(input())
+print(main(fruits,i))
